57306bbf47ab2a2da4f7aba10af9947e29d2fa88
Ashutosh Jena <ashutosh.jena@finbox.in>
2024-06-18 17:42:01 +0530

    Add project files

---
+++ RiskManager/RiskManager.h
@@ -0,0 +1,18 @@
+//
+//  RiskManager.h
+//  RiskManager
+//
+//  Created by Ashutosh Jena on 06/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RiskManager.
+FOUNDATION_EXPORT double RiskManagerVersionNumber;
+
+//! Project version string for RiskManager.
+FOUNDATION_EXPORT const unsigned char RiskManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RiskManager/PublicHeader.h>
+
+
