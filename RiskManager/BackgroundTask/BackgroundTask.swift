e62610c0f379756c7c978d924d347a2633f3b5fc
Ashutosh Jena <ashutosh.jena@finbox.in>
2024-07-03 20:22:46 +0530

    IRM-17 Add Comments

--- RiskManager/BackgroundTask/BackgroundTask.swift
+++ RiskManager/BackgroundTask/BackgroundTask.swift
@@ -19,53 +19,84 @@ extension FinBox {
     }
     
     // MARK: Register Background Tasks
-    public func setup() {
-        registerBackgroundTasks()
-    }
-
+    
+    /// Internal method to register background tasks and notifications.
     internal func registerBackgroundTasks() {
+        // Schedule the background refresh task
         scheduleBackgroundRefreshTask()
         
+        // Add observer for app resigning active state notification
         if #available(iOS 13.0, *) {
             NotificationCenter.default.addObserver(self, selector: #selector(willResignActive), name: UIScene.willDeactivateNotification, object: nil)
         } else {
             NotificationCenter.default.addObserver(self, selector: #selector(willResignActive), name: UIApplication.willResignActiveNotification, object: nil)
         }
     }
-
+    
+    
+    /// Public method to schedule a background refresh task.
     public func scheduleBackgroundRefreshTask() {
+        // Register the background task with a specific identifier
         BGTaskScheduler.shared.register(forTaskWithIdentifier: BACKGROUND_TASK_IDENTIFIER, using: nil) { task in
             debugPrint("App refresh task registered successfully")
+            
+            // Handle the app refresh task
             self.handleAppRefreshTask(task: task as! BGAppRefreshTask)
         }
     }
-
+    
+    
+    /// Handles the background app refresh task.
+    ///
+    /// - Parameter task: The background app refresh task (`BGAppRefreshTask`) to handle.
     func handleAppRefreshTask(task: BGAppRefreshTask) {
         debugPrint("App Refresh Task in handleAppRefreshTask")
+        
+        // Set an expiration handler for the task
         task.expirationHandler = { [weak self] in
+            // Reschedule the background refresh task
             self?.scheduleBackgroundRefreshTask()
+            
+            // Mark the task as completed
             task.setTaskCompleted(success: true)
         }
         
-        // Call the startPeriodicDataSync() method here
+        // Call the startPeriodicDataSync() method to initiate data synchronization
         self.startPeriodicDataSync()
         
+        // Schedule the background refresh task
         self.scheduleBackgroundRefreshTask()
+        
+        // Mark the task as completed
         task.setTaskCompleted(success: true)
     }
-
+    
+    
+    /// Handles the notification when the app is about to resign active state.
+    /// When an iOS app is said to "resign active state," it means that the app is no longer the foreground app and is about to transition into the background. This transition typically occurs in response to user actions, such as pressing the home button, receiving a phone call, or accessing the Control Center.
     @objc func willResignActive(_ notification: Notification) {
+        // Schedule data synchronization when the app is about to resign active state
         scheduleSyncData()
     }
-
+    
+    
+    
+    /// Schedules a background task to sync data with a specified frequency.
     func scheduleSyncData() {
+        // Get the current date
         let startDate = Date()
+        
+        // Get the current calendar object
         let calendar = Calendar.current
+        
+        // Calculate the date to schedule the task, adding syncFrequency seconds to startDate
         let date = calendar.date(byAdding: .second, value: Int(self.syncFrequency), to: startDate)
         
+        // Create a BGAppRefreshTaskRequest with a specific identifier
         let request = BGAppRefreshTaskRequest(identifier: BACKGROUND_TASK_IDENTIFIER)
-        request.earliestBeginDate = date
+        request.earliestBeginDate = date // Set the earliest begin date for the task
         
+        // Submit the task request to the BGTaskScheduler
         do {
             try BGTaskScheduler.shared.submit(request)
             debugPrint("BG Task submitted successfully with frequency: \(self.syncFrequency)")
@@ -73,7 +104,8 @@ extension FinBox {
             debugPrint("Unable to submit task: \(error.localizedDescription)")
         }
     }
-
+    
+    
     /// Sets the frequency for background sync tasks and saves it to SyncPref.
     ///
     /// - Parameters:
@@ -90,7 +122,7 @@ extension FinBox {
         default:
             syncFrequency = Double(value) // Default unit is seconds
         }
-
+        
         // The computed property `syncFrequency` will store the value in SyncPref
     }
     
