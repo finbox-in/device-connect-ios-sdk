2018db6d0639a077ba32eb845eb43921d61ac180
Ashutosh Jena <ashutosh.jena@finbox.in>
2024-08-14 19:53:57 +0530

    IRM-25 Add Errors

---
+++ RiskManager/Errors/RiskManagerError.swift
@@ -0,0 +1,12 @@
+//
+//  RiskManagerError.swift
+//  RiskManager
+//
+//  Created by Ashutosh Jena on 13/08/24.
+//
+
+import Foundation
+
+enum RiskManagerError: Error {
+    case empty(message: String)
+}
