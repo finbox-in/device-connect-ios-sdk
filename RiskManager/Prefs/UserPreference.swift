e258551630310ff0c61c7be6b3796d438bd030c4
Ashutosh Jena <ashutosh.jena@finbox.in>
2024-08-14 19:49:07 +0530

    IRM-19 Add Prefs

--- RiskManager/Prefs/UserPreference.swift
+++ RiskManager/Prefs/UserPreference.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-
 import Combine
 
 
@@ -14,16 +13,42 @@ import Combine
 class UserPreference {
     
     // Instance of user defaults
-    private let userDefaults = UserDefaults.standard
+    private let userDefaults = UserDefaults(suiteName: USER_DEFAULT_SUITE_ACCOUNT_DETAILS)
     
     // API Key
     var apiKey: String? {
         get {
-            return userDefaults.object(forKey: FINBOX_DEVICE_CONNECT_API_KEY) as? String ?? nil
+            return userDefaults?.object(forKey: FINBOX_DEVICE_CONNECT_API_KEY) as? String ?? nil
         }
         set {
-            userDefaults.set(newValue, forKey: FINBOX_DEVICE_CONNECT_API_KEY)
+            userDefaults?.set(newValue, forKey: FINBOX_DEVICE_CONNECT_API_KEY)
         }
     }
     
+    var userName: String? {
+        get {
+            return userDefaults?.object(forKey: USER_DEFAULT_USER_NAME) as? String ?? nil
+        }
+        set {
+            userDefaults?.set(newValue, forKey: USER_DEFAULT_USER_NAME)
+        }
+    }
+    
+    var userHash: String? {
+        get {
+            return userDefaults?.object(forKey: USER_DEFAULT_USER_HASH) as? String ?? nil
+        }
+        set {
+            userDefaults?.set(newValue, forKey: USER_DEFAULT_USER_HASH)
+        }
+    }
+    
+    var accessToken: String? {
+        get {
+            return userDefaults?.object(forKey: USER_DEFAULT_ACCESS_TOKEN) as? String ?? nil
+        }
+        set {
+            userDefaults?.set(newValue, forKey: USER_DEFAULT_ACCESS_TOKEN)
+        }
+    }
 }
