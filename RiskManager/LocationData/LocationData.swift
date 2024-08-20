b8a875e8411d7e4d2fc5902fb9ad0261e712479d
Srikar Reddy <gsrikar2010@gmail.com>
2024-08-20 09:23:00 +0530

    Make Location API from Location Data class

--- RiskManager/LocationData/LocationData.swift
+++ RiskManager/LocationData/LocationData.swift
@@ -8,6 +8,7 @@
 import Foundation
 import CoreLocation
 
+
 /**
  A utility class for fetching device location data asynchronously using the shared LocationManager instance.
  */
@@ -16,30 +17,81 @@ class LocationData {
     // Singleton instance
     let locationManager = LocationManager.shared
     
+    let accountSuite = UserPreference()
+    let syncSuite = SyncPref()
+    
+    init() {
+        
+    }
+    
+    private func getLocationEntity(location: CLLocation) -> LocationEntity {
+        let locationEntity = LocationEntity()
+        
+        if location.coordinate.latitude.isFinite {
+            locationEntity.latitude = location.coordinate.latitude
+        } else {
+            
+        }
+        
+        if location.coordinate.longitude.isFinite {
+            locationEntity.latitude = location.coordinate.longitude
+        } else {
+            
+        }
+        
+        if location.horizontalAccuracy.isFinite {
+            locationEntity.accuracy = location.horizontalAccuracy
+        } else {
+            
+        }
+        
+        if location.timestamp.timeIntervalSince1970.isFinite {
+            locationEntity.time = location.timestamp.timeIntervalSince1970
+        } else {
+            
+        }
+        
+        // Compute the location hash
+        if let username = accountSuite.userName {
+            let locationHash = username + String(describing: locationEntity.latitude) +
+            String(describing: locationEntity.longitude) + String(describing: locationEntity.time)
+            
+            // Compute the hash
+            locationEntity.id = CommonUtil.getMd5Hash(locationHash);
+        }
+        
+        return locationEntity
+    }
+    
+    private func getLocationModel(locationEntity: LocationEntity) -> LocationModel {
+        let locationModel = LocationModel()
+        locationModel.batchId = UUID().uuidString
+        locationModel.username = accountSuite.userName
+        locationModel.userHash = accountSuite.userHash
+        locationModel.sdkVersionName = VERSION_NAME
+        locationModel.syncId = syncSuite.syncId
+        locationModel.syncMechanism = syncSuite.syncMechanism
+        locationModel.isRealTime = syncSuite.isRealTime
+        locationModel.locationEntityList = [locationEntity]
+        return locationModel
+    }
+    
     /**
      Fetches location data asynchronously and provides it via a completion handler
      */
-    func getLocationData(completion: @escaping ([String: Any]) -> Void) {
-        // Call LocationManager's getLocationData method to retrieve current location
+    func syncLocationData() {
+        // Call LocationManager to retrieve current location
         locationManager.getLocationData { location in
-            var locationData: [String: Any] = [:]
-            guard let location = location else {
-                print("Failed to get location")
-                completion(locationData)
-                return
+            if let location = location {
+                
+                // Create the location variable
+                let locationEntity = self.getLocationEntity(location: location)
+                
+                let locationModel = self.getLocationModel(locationEntity: locationEntity)
+                
+                // Send the data to the server
+                APIService.instance.syncLocationData(data: locationModel, syncItem: SyncType.LOCATION)
             }
-            if location.coordinate.latitude.isFinite {
-                locationData[LATITUDE_KEY] = location.coordinate.latitude
-            } else {
-                locationData[LATITUDE_KEY] = "Latitude not available"
-            }
-            
-            if location.coordinate.longitude.isFinite {
-                locationData[LONGITUDE_KEY] = location.coordinate.longitude
-            } else {
-                locationData[LONGITUDE_KEY] = "Longitude not available"
-            }
-            completion(locationData)
         }
     }
     
